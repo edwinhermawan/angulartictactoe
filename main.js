9745746010a1eeabda7ffda48ba409236d3fe3df
edwinhermawan <edwin@signaturepages.com>
2013-10-02 17:57:11 -0700

    Xs and Os appear

---
+++ main.js
@@ -0,0 +1,8 @@
+ function FirstController ($scope){
+
+ }
+
+ this.$watch('cell', function(mark){
+ 	// if the element last added was an X then put an O
+ 	// if the element last added was an O then put an X
+ })
